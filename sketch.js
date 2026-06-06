494c6bd9fcf5beb3e4d644130fa9e6ef380c21e8
Wenjia Jiang <13877189708@163.com>
2026-06-06 22:08:52 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,16 @@
 let gameState = "home";
-
 let score = 0;
 
 let objects = [];
-let particles = [];
 
 let playButton;
-
 let gardenButton;
 let hunterButton;
 let bubbleButton;
 
 function setup() {
-
-  createCanvas(
-    windowWidth,
-    windowHeight
-  );
-
-  textAlign(
-    CENTER,
-    CENTER
-  );
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
 
   playButton = {
     x: width / 2,
@@ -32,68 +21,46 @@ function setup() {
 
   gardenButton = {
     x: width / 2,
-    y: height / 2 + 40,
+    y: height / 2 + 20,
     w: 220,
     h: 55
   };
 
   hunterButton = {
     x: width / 2,
-    y: height / 2 + 110,
+    y: height / 2 + 95,
     w: 220,
     h: 55
   };
 
   bubbleButton = {
     x: width / 2,
-    y: height / 2 + 180,
+    y: height / 2 + 170,
     w: 220,
     h: 55
   };
-
-  cursor(HAND);
 }
 
 function draw() {
 
-  if (
-    gameState === "home"
-  ) {
-
+  if (gameState === "home") {
     drawHome();
-
   }
 
-  if (
-    gameState === "menu"
-  ) {
-
+  if (gameState === "menu") {
     drawMenu();
-
   }
 
-  if (
-    gameState === "garden"
-  ) {
-
+  if (gameState === "garden") {
     drawGarden();
-
   }
 
-  if (
-    gameState === "hunter"
-  ) {
-
+  if (gameState === "hunter") {
     drawHunter();
-
   }
 
-  if (
-    gameState === "bubble"
-  ) {
-
+  if (gameState === "bubble") {
     drawBubble();
-
   }
 
   drawPawCursor();
@@ -101,97 +68,230 @@ function draw() {
 
 function drawHome() {
 
-  background(
-    250,
-    248,
-    242
-  );
+  background(250,248,242);
 
   fill(50);
-
-  textSize(72);
-
-  text(
-    "🐾",
-    width / 2,
-    height / 2 - 120
-  );
+  textSize(70);
+  text("🐾", width/2, height/2-120);
 
   textSize(52);
-
-  text(
-    "PAWPLAY",
-    width / 2,
-    height / 2 - 40
-  );
+  text("PAWPLAY", width/2, height/2-40);
 
   fill(120);
-
   textSize(18);
-
   text(
     "Touch • Chase • Explore",
-    width / 2,
-    height / 2 + 5
+    width/2,
+    height/2+10
   );
 
   drawButton(
     playButton,
     "PLAY",
-    color(
-      255,
-      210,
-      80
-    )
+    color(255,210,80)
   );
 }
 
 function drawMenu() {
 
-  background(
-    250,
-    248,
-    242
-  );
+  background(250,248,242);
 
   fill(50);
-
   textSize(42);
-
   text(
     "Choose a Mode",
-    width / 2,
-    height / 2 - 80
+    width/2,
+    height/2-80
   );
 
   drawButton(
     gardenButton,
     "🌼 Sunny Garden",
-    color(
-      255,
-      220,
-      90
-    )
+    color(255,220,90)
   );
 
   drawButton(
     hunterButton,
     "🌙 Night Hunter",
-    color(
-      240,
-      240,
-      100
-    )
+    color(240,240,120)
   );
 
   drawButton(
     bubbleButton,
-    "🌊 Bubble Bay",
-    color(
-      120,
-      190,
-      255
-    )
+    "🫧 Bubble Bay",
+    color(120,190,255)
+  );
+}
+
+function drawGarden() {
+
+  background(255);
+
+  drawTopBar();
+
+  if(frameCount % 40 === 0){
+    spawnObject(["🌼","🐝","💛"]);
+  }
+
+  updateObjects();
+}
+
+function drawHunter() {
+
+  background(20);
+
+  drawTopBar();
+
+  if(frameCount % 40 === 0){
+    spawnObject(["⭐","✨","🌙"]);
+  }
+
+  updateObjects();
+}
+
+function drawBubble() {
+
+  background(180,220,255);
+
+  drawTopBar();
+
+  if(frameCount % 40 === 0){
+    spawnObject(["🫧","🐟","💧"]);
+  }
+
+  updateObjects();
+}
+
+function drawTopBar(){
+
+  fill(255);
+  stroke(0);
+
+  rect(20,20,100,40,20);
+
+  fill(0);
+  noStroke();
+
+  textSize(18);
+  text("← Back",70,40);
+
+  fill(255);
+  stroke(0);
+
+  rect(width-120,20,100,40,20);
+
+  fill(0);
+  noStroke();
+
+  text(
+    "⭐ "+score,
+    width-70,
+    40
+  );
+}
+
+function spawnObject(list){
+
+  let emoji =
+    random(list);
+
+  objects.push({
+    x: random(width),
+    y: random(height),
+    e: emoji,
+    size: random(35,55)
+  });
+}
+
+function updateObjects(){
+
+  textSize(40);
+
+  for(let i=objects.length-1;i>=0;i--){
+
+    let o = objects[i];
+
+    textSize(o.size);
+    text(o.e,o.x,o.y);
+  }
+}
+
+function mousePressed(){
+
+  if(gameState==="home"){
+
+    if(overButton(playButton)){
+      gameState="menu";
+    }
+  }
+
+  else if(gameState==="menu"){
+
+    if(overButton(gardenButton)){
+      gameState="garden";
+      objects=[];
+      score=0;
+    }
+
+    if(overButton(hunterButton)){
+      gameState="hunter";
+      objects=[];
+      score=0;
+    }
+
+    if(overButton(bubbleButton)){
+      gameState="bubble";
+      objects=[];
+      score=0;
+    }
+  }
+
+  else{
+
+    if(
+      mouseX>20 &&
+      mouseX<120 &&
+      mouseY>20 &&
+      mouseY<60
+    ){
+      gameState="menu";
+      objects=[];
+      return;
+    }
+
+    for(
+      let i=objects.length-1;
+      i>=0;
+      i--
+    ){
+
+      let d=
+      dist(
+        mouseX,
+        mouseY,
+        objects[i].x,
+        objects[i].y
+      );
+
+      if(d<40){
+
+        objects.splice(i,1);
+
+        score++;
+
+        break;
+      }
+    }
+  }
+}
+
+function overButton(btn){
+
+  return (
+    mouseX > btn.x-btn.w/2 &&
+    mouseX < btn.x+btn.w/2 &&
+    mouseY > btn.y-btn.h/2 &&
+    mouseY < btn.y+btn.h/2
   );
 }
 
@@ -199,12 +299,10 @@ function drawButton(
   btn,
   label,
   c
-) {
+){
 
   fill(c);
-
   stroke(255);
-
   strokeWeight(2);
 
   rectMode(CENTER);
@@ -230,7 +328,7 @@ function drawButton(
   );
 }
 
-function drawPawCursor() {
+function drawPawCursor(){
 
   noCursor();
 
@@ -240,5 +338,12 @@ function drawPawCursor() {
     "🐾",
     mouseX,
     mouseY
+  );
+}
+
+function windowResized(){
+  resizeCanvas(
+    windowWidth,
+    windowHeight
   );
 }
